6e8766c989a7de9292aed745daf69727581e28b9
@ZhangBo <yanerlove926@163.com>
2016-04-27 14:34:06 +0800

    Initial Commit

---
+++ BYKit/BYKit.h
@@ -0,0 +1,19 @@
+//
+//  BYKit.h
+//  BYKit
+//
+//  Created by SunShine.Rock on 16/4/27.
+//  Copyright © 2016年 Berton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BYKit.
+FOUNDATION_EXPORT double BYKitVersionNumber;
+
+//! Project version string for BYKit.
+FOUNDATION_EXPORT const unsigned char BYKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BYKit/PublicHeader.h>
+
+
